367b43816545e4054e834c73447c5d57a3ccc41c
Konrad <konrad.kipczak@gmail.com>
2018-02-07 18:51:40 +0000

    created temporary hardcoded data for testing

---
+++ public/scripts/tempDB.js
@@ -0,0 +1,23 @@
+user = {
+	'01' : {
+		id : 01,
+		name : 'tester'
+	}
+};
+
+todoList = {
+	'0a1' : {
+		id : '0a1',
+		userID : '01',
+		category : '1',
+		name : 'Robocop',
+		dateCreated : '2018-02-07'
+	}
+};
+
+categories = {
+	'1' : 'Films/Tv Series',
+	'2' : 'Books',
+	'3' : 'Restaurants',
+	'4' : 'Products'
+};
