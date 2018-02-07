73c874e84de2b82cdd31f5f8b5bdb20c5f1dd1c6
Konrad <konrad.kipczak@gmail.com>
2018-02-07 20:36:31 +0000

    created schema for Knex database

---
+++ db/migrations/20180207200841_create_tables_for_ToDo_and_categories.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('categories', function (table) {
+    table.increments('id');
+    table.string('name');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table('categories', function (table) {
+    table.dropColumn('id');
+    table.dropColumn ('name');
+  });
+};
