66a5c0af1da4699e299f71b1d1134307c2379b2a
Konrad <konrad.kipczak@gmail.com>
2018-02-08 22:46:45 +0000

    using localstorage for user logins

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,25 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(let user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });
+// $(() => {
+//   $.ajax({
+//     method: "GET",
+//     url: "/api/users"
+//   }).done((users) => {
+//     for(let user of users) {
+//       $("<div>").text(user.name).appendTo($("body"));
+//     }
+//   });
+// });
+
+
+
+
+$(document).ready(function() {
+
+if (!localStorage.logged) {
+    $('#newToDo').css('display', 'none');
+    $('#logout').css('display', 'none');
+} else {
+    $('#loginB').css('display', 'none');
+    $('#regB').css('display', 'none');
+}
+
 });
