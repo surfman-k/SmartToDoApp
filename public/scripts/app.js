ee02af39190b9b2a859045ec1c59d64b40fe7f14
Konrad <konrad.kipczak@gmail.com>
2018-02-12 04:59:02 +0000

    final cleanup

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,72 +1,74 @@
 $(document).ready(function() {
-
-    // Injects data into columns
-   $.ajax({
-    method: "GET",
-    url: "/api/todoList"
-  }).done((todolist) => {
-    for(let obj of todolist) {
-      if(obj.user == localStorage.getItem("user")){
+  // Injects data into columns
+  $.ajax({
+    method: 'GET',
+    url: '/api/todoList',
+  }).done(todolist => {
+    for (let obj of todolist) {
+      if (obj.user == localStorage.getItem('user')) {
         if (obj.category === 1) {
-          if(obj.checked === false ) {
-            $(".movies .list-group").prepend(renderToDoElement(obj, false));
+          if (obj.checked === false) {
+            $('.movies .list-group').prepend(renderToDoElement(obj, false));
           } else {
-            $(".movies .done" ).prepend(renderToDoElement(obj, true));
+            $('.movies .done').prepend(renderToDoElement(obj, true));
           }
         }
-          if (obj.category === 2) {
-            if(obj.checked === false ) {
-              $(".books .list-group").prepend(renderToDoElement(obj, false));
-            } else {
-              $(".books .done" ).prepend(renderToDoElement(obj, true));
-            }
+        if (obj.category === 2) {
+          if (obj.checked === false) {
+            $('.books .list-group').prepend(renderToDoElement(obj, false));
+          } else {
+            $('.books .done').prepend(renderToDoElement(obj, true));
           }
+        }
 
-          if (obj.category === 3) {
-            if(obj.checked === false ) {
-              $(".rest .list-group").prepend(renderToDoElement(obj, false));
-            } else {
-              $(".rest .done" ).prepend(renderToDoElement(obj, true));
-            }
+        if (obj.category === 3) {
+          if (obj.checked === false) {
+            $('.rest .list-group').prepend(renderToDoElement(obj, false));
+          } else {
+            $('.rest .done').prepend(renderToDoElement(obj, true));
           }
+        }
 
-          if (obj.category === 4) {
-            if(obj.checked === false ) {
-              $(".product .list-group").prepend(renderToDoElement(obj, false));
-            } else {
-              $(".product .done" ).prepend(renderToDoElement(obj, true));
-            }
+        if (obj.category === 4) {
+          if (obj.checked === false) {
+            $('.product .list-group').prepend(renderToDoElement(obj, false));
+          } else {
+            $('.product .done').prepend(renderToDoElement(obj, true));
           }
         }
       }
-    });
+    }
+  });
 
   function renderToDoElement(data, boolean) {
-
-    let $toDoMain = $("<a>").attr("href", "#")
-                            .addClass("list-group-item list-group-item-action flex-column align-items-start")
-                            .attr("ident", data.id);
-    let $mainContainer = $("<div>").addClass("d-flex w-100 justify-content-between");
+    let $toDoMain = $('<a>')
+      .attr('href', '#')
+      .addClass(
+        'list-group-item list-group-item-action flex-column align-items-start'
+      )
+      .attr('ident', data.id);
+    let $mainContainer = $('<div>').addClass(
+      'd-flex w-100 justify-content-between'
+    );
 
     $toDoMain.append($mainContainer);
 
-    let $toDoTitle = $("<h5>").addClass("mb-1").text(data.name);
+    let $toDoTitle = $('<h5>').addClass('mb-1').text(data.name);
 
     $mainContainer.append($toDoTitle);
 
-    let $comment = $("<p>").addClass("mb-1").text(data.comment);
+    let $comment = $('<p>').addClass('mb-1').text(data.comment);
 
     $toDoMain.append($comment);
 
-    let $creationDate = $("<small>").text(data.createdOn.substring(10, -1));
+    let $creationDate = $('<small>').text(data.createdOn.substring(10, -1));
 
     $toDoMain.append($creationDate);
 
     if (boolean === true) {
-
-      let $doneContainer = $("<div>");
-      let $doneH3 = $("<h3>").text("Done:");
-      let $doneDiv = $("<div>").addClass(".finished");
+      let $doneContainer = $('<div>');
+      let $doneH3 = $('<h3>').text('Done:');
+      let $doneDiv = $('<div>').addClass('.finished');
 
       $doneContainer.append($doneH3);
       $doneDiv.append($toDoMain);
@@ -75,123 +77,53 @@ $(document).ready(function() {
       return $doneContainer;
     }
 
-    // Edit buttons
-      // let $editButton = $("<button>").attr({
-      // type: 'submit',
-      // class: 'btn btn-default btn-sm'})
-      // .text("update")
-      // .addClass('Testing');
-
-      // let $testDiv = $("<div>").addClass('testingDiv')
-
-      // $testDiv.append($editButton).hide();
-      // $toDoMain.append($testDiv);
-
     return $toDoMain;
   }
 
-
-  let $newToDo = $("#loginB");
-  $newToDo.on("click", function(event) {
+  let $newToDo = $('#loginB');
+  $newToDo.on('click', function(event) {
     event.preventDefault();
-
   });
 
   if (!localStorage.user) {
-    $('html').css("background-image", "url(/image/bg3.png)");
+    $('html').css('background-image', 'url(/image/bg3.png)');
     $('#newToDo').css('display', 'none');
     $('#logout').css('display', 'none');
     $('#main').css('opacity', '0');
   } else {
-    $('html').css("background-image", "url(/image/bg2.png)");
+    $('html').css('background-image', 'url(/image/bg2.png)');
     $('#loginB').css('display', 'none');
     $('#regB').css('display', 'none');
   }
 
-
-  $(".registerLink").on("click", function(event) {
+  $('.registerLink').on('click', function(event) {
     event.preventDefault();
-    $("#id01").css('display', 'none');
-    $("#id03").css('display', 'block');
+    $('#id01').css('display', 'none');
+    $('#id03').css('display', 'block');
     // $("#id04").css('display', 'block')
   });
 
   let uniqueElemId;
 
-  $(document).on("click", ".list-group-item", function(event) {
+  $(document).on('click', '.list-group-item', function(event) {
     event.preventDefault();
-    uniqueElemId= $(this).attr('ident');
-    $("#id04").css('display', 'block')
+    uniqueElemId = $(this).attr('ident');
+    $('#id04').css('display', 'block');
   });
-
-
-  // Edit comment
-
-  // $("p .mb-1").on("click", function() {
-  //     let $this = $(this);
-  //     let $input = $('<input>', {
-  //         value: $this.text(),
-  //         type: 'text',
-  //         blur: function() {
-  //            $this.text(this.value);
-  //         },
-  //         keyup: function(e) {
-  //            if (e.which === 13) $input.blur();
-  //         }
-  //     }).appendTo($this.empty()).focus();
-  // });
-
-
-  // //Expand element
-  // $(document).on("click", ".list-group-item", function(event) {
-
-  //   if ($(this).height() !== 250) {
-  //         $(this).animate({ height: 250 }, 1000 );
-  //         $('.TestingDiv').show();
-  //   event.preventDefault();
-  //       }
-  //   else {
-  //         $( this ).animate({ height: 124.5 }, 1000 );
-  //       }
-  // });
-
-  // $("#regB").on("click", function(event) {
-  //   event.preventDefault();
-  //   $("id03").modal('hide');
-  // });
-
-  //Registration validator
-
-  // $("#registerButton").on("click", function(event) {
-  //   event.preventDefault();
-  //     let $passwordReg = $("#passwordReg").val();
-  //     let $confirmPass = $("#passwordConfirm").val();
-
-  //     if (($passwordReg.length || $confirmPass.length) === 0) {
-  //       $.flash("nothing entered")
-  //     } else if ($passwordReg !== $confirmPass) {
-  //       $.flash("Passwords do not match :(")
-  //     } else {
-  //       alert("Woo. A match!");
-  //     }
-
-  // });
-
 
   //AJAX for login form
   $('.loginForm').submit(function(event) {
     event.preventDefault();
-    let uname = ($('#username').val());
-    let psw = ($('#inputPassword3').val());
+    let uname = $('#username').val();
+    let psw = $('#inputPassword3').val();
 
     $.ajax({
-    type: "POST",
-    url: "/login",
-    async: true,
-    data: {uname: uname, psw: psw}
-    })
-    .done(function(data){
-      if(Number.isInteger(data.id)){
+      type: 'POST',
+      url: '/login',
+      async: true,
+      data: { uname: uname, psw: psw },
+    }).done(function(data) {
+      if (Number.isInteger(data.id)) {
         localStorage.setItem('user', data.id);
         location.reload();
       } else {
@@ -201,20 +133,19 @@ $(document).ready(function() {
   });
 
   //AJAX for registration form
-   $('.regForm').submit(function(event) {
+  $('.regForm').submit(function(event) {
     event.preventDefault();
-    let uname = ($('#reguname').val());
-    let psw = ($('#passwordReg').val());
+    let uname = $('#reguname').val();
+    let psw = $('#passwordReg').val();
 
     $.ajax({
-    type: "POST",
-    url: "/reg",
-    async: true,
-    data: {uname: uname, psw: psw}
-    })
-    .done(function(data){
-      if(Number.isInteger(data[0])){
-        localStorage.setItem("user", data[0]);
+      type: 'POST',
+      url: '/reg',
+      async: true,
+      data: { uname: uname, psw: psw },
+    }).done(function(data) {
+      if (Number.isInteger(data[0])) {
+        localStorage.setItem('user', data[0]);
         location.reload();
       } else {
         alert('Username already exists!');
@@ -223,62 +154,65 @@ $(document).ready(function() {
   });
 
   //AJAX for logout button
-   $('#logout').click(function(event) {
+  $('#logout').click(function(event) {
     event.preventDefault();
-    localStorage.removeItem("user");
+    localStorage.removeItem('user');
     location.reload();
   });
 
-
   // AJAX for adding a ToDo Item
-   $('.toDoForm').submit(function(event) {
+  $('.toDoForm').submit(function(event) {
     event.preventDefault();
-    let tdname = ($('#tdname').val());
-    let todoUser = localStorage.getItem("user");
+    let tdname = $('#tdname').val();
+    let todoUser = localStorage.getItem('user');
     let today = new Date();
-    let dateCreated = (`${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`);
-    let commentInput = $("#commentInput").val();
+    let dateCreated = `${today.getFullYear()}-${today.getMonth() +
+      1}-${today.getDate()}`;
+    let commentInput = $('#commentInput').val();
 
     $.ajax({
-    type: "POST",
-    url: "/newToDo",
-    async: false,
-    data: {name: tdname, user: todoUser, category: 3, createdOn: dateCreated, comment: commentInput, checked: false}
-    })
-    .done(location.reload());
+      type: 'POST',
+      url: '/newToDo',
+      async: false,
+      data: {
+        name: tdname,
+        user: todoUser,
+        category: 3,
+        createdOn: dateCreated,
+        comment: commentInput,
+        checked: false,
+      },
+    }).done(location.reload());
   });
 
-   let editCategory;
-   $('#categoryChanger a').on('click', function(event){
-     event.preventDefault();
-     editCategory = $(this).attr('value');
-    });
+  let editCategory;
+  $('#categoryChanger a').on('click', function(event) {
+    event.preventDefault();
+    editCategory = $(this).attr('value');
+  });
 
   // AJAX for Edit Form
-  $("#editSubmit").on("click", function(event) {
+  $('#editSubmit').on('click', function(event) {
     event.preventDefault();
-    let commentEdit = ($('#commentEdit').val());
+    let commentEdit = $('#commentEdit').val();
 
     $.ajax({
-    type: "POST",
-    url: "/editToDo",
-    async: false,
-    data: {id: uniqueElemId, category: editCategory, comment: commentEdit}
-    })
-    .done(location.reload());
+      type: 'POST',
+      url: '/editToDo',
+      async: false,
+      data: { id: uniqueElemId, category: editCategory, comment: commentEdit },
+    }).done(location.reload());
   });
 
   // AJAX for Delete
-  $("#deleteElem").on("click", function(event) {
+  $('#deleteElem').on('click', function(event) {
     event.preventDefault();
 
     $.ajax({
-    type: "POST",
-    url: "/delete",
-    async: false,
-    data: {id: uniqueElemId}
-    })
-    .done(location.reload());
+      type: 'POST',
+      url: '/delete',
+      async: false,
+      data: { id: uniqueElemId },
+    }).done(location.reload());
   });
-
 });
--- server.js
+++ server.js
@@ -1,29 +1,29 @@
-"use strict";
+'use strict';
 
 require('dotenv').config();
 
-const PORT        = process.env.PORT || 8080;
-const ENV         = process.env.ENV || "development";
-const express     = require("express");
-const bodyParser  = require("body-parser");
-const sass        = require("node-sass-middleware");
-const app         = express();
+const PORT = process.env.PORT || 8080;
+const ENV = process.env.ENV || 'development';
+const express = require('express');
+const bodyParser = require('body-parser');
+const sass = require('node-sass-middleware');
+const app = express();
 
-const knexConfig  = require("./knexfile");
-const knex        = require("knex")(knexConfig[ENV]);
-const morgan      = require('morgan');
-const knexLogger  = require('knex-logger');
-const bcrypt 	  = require('bcrypt');
-const jwt		  = require('jsonwebtoken');
-const APIClinet   = require('omdb-api-client');
-const amazon      = require('amazon-product-api');
+const knexConfig = require('./knexfile');
+const knex = require('knex')(knexConfig[ENV]);
+const morgan = require('morgan');
+const knexLogger = require('knex-logger');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const APIClinet = require('omdb-api-client');
+const amazon = require('amazon-product-api');
 
 const GooglePlacesPromises = require('googleplaces-promises');
 
 // Seperated Routes for each Resource
-const usersRoutes = require("./routes/users");
-const todoListRoutes = require("./routes/todoList");
-const categoryRoutes = require("./routes/category");
+const usersRoutes = require('./routes/users');
+const todoListRoutes = require('./routes/todoList');
+const categoryRoutes = require('./routes/category');
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -35,11 +35,10 @@ let omdb = new APIClinet();
 let client = amazon.createClient({
   awsId: process.env.awsId,
   awsSecret: process.env.awsSecret,
-  awsTag: "todo04c-20"
+  awsTag: 'todo04c-20',
 });
 
 let placesPromises = new GooglePlacesPromises(process.env.googleAPIkey);
-
 
 // Bootstrap
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
@@ -49,171 +48,229 @@ app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 // Log knex SQL queries to STDOUT as well
 app.use(knexLogger(knex));
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/styles", sass({
-  src: __dirname + "/styles",
-  dest: __dirname + "/public/styles",
-  debug: true,
-  outputStyle: 'expanded'
-}));
+app.use(
+  '/styles',
+  sass({
+    src: __dirname + '/styles',
+    dest: __dirname + '/public/styles',
+    debug: true,
+    outputStyle: 'expanded',
+  })
+);
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // Mount all resource routes
-app.use("/api/users", usersRoutes(knex));
-app.use("/api/todoList", todoListRoutes(knex));
-app.use("/api/category", categoryRoutes(knex));
+app.use('/api/users', usersRoutes(knex));
+app.use('/api/todoList', todoListRoutes(knex));
+app.use('/api/category', categoryRoutes(knex));
 
 // Home page
-app.get("/", (req, res) => {
-  res.render("index");
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
 //Registration functionality
-app.post("/reg", (data, res) => {
-	let textpass = data.body.psw;
-	let username = data.body.uname;
-	let hashed = bcrypt.hashSync(textpass, 10);
+app.post('/reg', (data, res) => {
+  let textpass = data.body.psw;
+  let username = data.body.uname;
+  let hashed = bcrypt.hashSync(textpass, 10);
 
-	let insert1 = {name: username, password: hashed};
+  let insert1 = { name: username, password: hashed };
 
-	knex.insert(insert1)
-	.returning('id')
-	.into('users')
-	.then(function (id) {
-		let payload = {user : data.body.uname};
-		jwt.sign(payload, process.env.secretKEY, function(err, token) {
-  			console.log(token);
-  			res.status(201).send(id);
-		});
-	})
-	.catch(function(error) {
-  		res.status(201).send("USERNAME EXISTS!!!");
-	});
-
+  knex
+    .insert(insert1)
+    .returning('id')
+    .into('users')
+    .then(function(id) {
+      let payload = { user: data.body.uname };
+      jwt.sign(payload, process.env.secretKEY, function(err, token) {
+        console.log(token);
+        res.status(201).send(id);
+      });
+    })
+    .catch(function(error) {
+      res.status(201).send('USERNAME EXISTS!!!');
+    });
 });
 
 //Login functionality
-app.post("/login", (data, res) => {
-
-	knex('users').where({name: data.body.uname}).select('*').then(function(id) {
-		return {id: id[0].id, name: id[0].name, psw: id[0].password};
-	})
-	.then(function(pass){
-		if(bcrypt.compareSync(data.body.psw, pass.psw)){
-			let payload = {user : pass.name};
-			console.log(payload);
-			jwt.sign(payload, process.env.secretKEY, function(err, token) {
-  				console.log(token);
-  				res.status(201).send({id: pass.id, token: token});
-			});
-		} else {
-			res.status(201).send('password wrong!');
-		}
-	})
-	.catch(function(error) {
-		res.status(201).send('password wrong!');
-	});
+app.post('/login', (data, res) => {
+  knex('users')
+    .where({ name: data.body.uname })
+    .select('*')
+    .then(function(id) {
+      return { id: id[0].id, name: id[0].name, psw: id[0].password };
+    })
+    .then(function(pass) {
+      if (bcrypt.compareSync(data.body.psw, pass.psw)) {
+        let payload = { user: pass.name };
+        console.log(payload);
+        jwt.sign(payload, process.env.secretKEY, function(err, token) {
+          console.log(token);
+          res.status(201).send({ id: pass.id, token: token });
+        });
+      } else {
+        res.status(201).send('password wrong!');
+      }
+    })
+    .catch(function(error) {
+      res.status(201).send('password wrong!');
+    });
 });
 
 //Editing a ToDo
-app.post("/editToDo", (data, res) => {
-  let changes = {id: data.body.id, category: data.body.category, comment: data.body.comment};
-  knex("todolist").where("id", data.body.id)
+app.post('/editToDo', (data, res) => {
+  let changes = {
+    id: data.body.id,
+    category: data.body.category,
+    comment: data.body.comment,
+  };
+  knex('todolist')
+    .where('id', data.body.id)
     .update(changes)
-    .then(function (error) {
+    .then(function(error) {
       console.error(error);
-    }); res.redirect("/");
+    });
+  res.redirect('/');
 });
 
-
 //Creating a new ToDo
-app.post("/newToDo", (data, res) => {
+app.post('/newToDo', (data, res) => {
+  let input = data.body.name;
+  let flag = 1;
 
-	let input = data.body.name;
-	let flag = 1;
+  placesPromises
+    .placeSearch({
+      keyword: input,
+      type: ['food'],
+      location: [45.4961, -73.5693],
+      radius: '5000',
+    })
+    .then(function(response) {
+      if (response.results.length > 0) {
+        if (flag === 1) {
+          let insert1 = {
+            name: data.body.name,
+            user: data.body.user,
+            category: 3,
+            createdOn: data.body.createdOn,
+            comment: data.body.comment,
+            checked: false,
+          };
+          flag = 5;
+          knex
+            .insert(insert1)
+            .into('todolist')
+            .then(function(id) {})
+            .catch(function(error) {
+              console.error(error.detail);
+            })
+            .then(function() {});
+          res.redirect('/');
+        }
+      }
+    })
+    .then(function() {
+      omdb({ t: input, apikey: process.env.omdbAPI })
+        .list()
+        .then(function(movie) {
+          if (movie.imdbRating > 6) {
+            flag = 5;
+            let insert1 = {
+              name: data.body.name,
+              user: data.body.user,
+              category: 1,
+              createdOn: data.body.createdOn,
+              comment: data.body.comment,
+              checked: false,
+            };
 
+            knex
+              .insert(insert1)
+              .into('todolist')
+              .then(function(id) {})
+              .catch(function(error) {
+                console.error(error.detail);
+              })
+              .then(function() {});
+            res.redirect('/');
+          }
+        });
+    })
+    .then(function() {
+      client.itemSearch({ Keywords: input }).then(function(results) {
+        if (results[0].ItemAttributes[0].ProductGroup[0] == 'Book') {
+          if (flag === 1) {
+            let insert1 = {
+              name: data.body.name,
+              user: data.body.user,
+              category: 2,
+              createdOn: data.body.createdOn,
+              comment: data.body.comment,
+              checked: false,
+            };
+            flag = 5;
+            knex
+              .insert(insert1)
+              .into('todolist')
+              .then(function(id) {})
+              .catch(function(error) {
+                console.error(error.detail);
+              })
+              .then(function() {});
+            res.redirect('/');
+          }
+        }
+      });
+    })
+    .then(function() {
+      client
+        .itemSearch({ Keywords: input })
+        .then(function(results) {
+          if (results[0].ItemAttributes[0].ProductGroup[0] != 'Book') {
+            if (flag === 1) {
+              let insert1 = {
+                name: data.body.name,
+                user: data.body.user,
+                category: 4,
+                createdOn: data.body.createdOn,
+                comment: data.body.comment,
+                checked: false,
+              };
 
-		placesPromises.placeSearch({keyword: input, type: ['food'], location: [45.4961,-73.5693], radius: "5000"}).then(function(response){
-        	if(response.results.length > 0){
-
-        	if(flag === 1){
-				let insert1 = {name: data.body.name, user: data.body.user, category: 3, createdOn: data.body.createdOn, comment: data.body.comment, checked: false };
-				flag = 5;
-				knex.insert(insert1).into("todolist").then(function (id) {})
-				.catch(function(error) {
-			  		console.error(error.detail);
-				}).then(function(){});
-				res.redirect('/');
-				}
-
-        		}
-    		}).then( function () {
-		omdb({t: input, apikey: process.env.omdbAPI}).list().then(function(movie) {
-			if(movie.imdbRating > 6){
-				flag = 5;
-				let insert1 = {name: data.body.name, user: data.body.user, category: 1, createdOn: data.body.createdOn, comment: data.body.comment, checked: false };
-
-				knex.insert(insert1).into("todolist").then(function (id) {})
-				.catch(function(error) {
-			  		console.error(error.detail);
-				})
-				.then(function(){});
-				res.redirect('/');
-			}
-		})
-		}).then( function () {
-    	client.itemSearch({Keywords: input}).then(function(results){
-		  	if(results[0].ItemAttributes[0].ProductGroup[0] == 'Book'){
-
-		  	if(flag === 1){
-				let insert1 = {name: data.body.name, user: data.body.user, category: 2, createdOn: data.body.createdOn, comment: data.body.comment, checked: false };
-				flag = 5;
-				knex.insert(insert1).into("todolist").then(function (id) {})
-				.catch(function(error) {
-			  		console.error(error.detail);
-				})
-				.then(function(){});
-				res.redirect('/');
-				}
-			}
-		})
-		}).then( function(){
-		client.itemSearch({Keywords: input}).then(function(results){
-		  	if(results[0].ItemAttributes[0].ProductGroup[0] != 'Book'){
-
-		  	if(flag === 1){
-				let insert1 = {name: data.body.name, user: data.body.user, category: 4, createdOn: data.body.createdOn, comment: data.body.comment, checked: false };
-
-				knex.insert(insert1).into("todolist").then(function (id) {})
-				.catch(function(error) {
-			  		console.error(error.detail);
-				})
-				.then(function(){});
-				res.redirect('/');
-				}
-			}
-			}).catch(function(err){
-				console.log(err.Error);
-			})})
-		.catch(function(err) {
-		    console.log(err);
-		});
-
+              knex
+                .insert(insert1)
+                .into('todolist')
+                .then(function(id) {})
+                .catch(function(error) {
+                  console.error(error.detail);
+                })
+                .then(function() {});
+              res.redirect('/');
+            }
+          }
+        })
+        .catch(function(err) {
+          console.log(err.Error);
+        });
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 });
 
 //Deleting a todo
-app.post("/delete", (data, res) => {
-  let deletionId = {id: data.body.id};
-  knex("todolist").where("id", data.body.id)
-    .del()
-    .then(function (error) {
-      console.error(error);
-    }); res.redirect("/");
+app.post('/delete', (data, res) => {
+  let deletionId = { id: data.body.id };
+  knex('todolist').where('id', data.body.id).del().then(function(error) {
+    console.error(error);
+  });
+  res.redirect('/');
 });
 
-
 app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
+  console.log('Example app listening on port ' + PORT);
 });
